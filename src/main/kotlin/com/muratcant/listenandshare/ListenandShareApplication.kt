f0bffee2cff8b56e9dbab639c15b5b5b4753d522
Murat Can Tanriverdi <muratcantanriverdi96@gmail.com>
2023-01-01 13:59:56 +0300

    init project

---
+++ src/main/kotlin/com/muratcant/listenandshare/ListenandShareApplication.kt
@@ -0,0 +1,11 @@
+package com.muratcant.listenandshare
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ListenandShareApplication
+
+fun main(args: Array<String>) {
+	runApplication<ListenandShareApplication>(*args)
+}
---
+++ src/test/kotlin/com/muratcant/listenandshare/ListenandShareApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.muratcant.listenandshare
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ListenandShareApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
